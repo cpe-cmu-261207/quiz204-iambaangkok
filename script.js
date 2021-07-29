30a435908f147712c60d45983674517752f67bde
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-07-29 14:20:22 +0700

    finished

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+
+const text = document.querySelector("#text");
+const defaultText = text.innerHTML;
+var words = [];
+var defColor = color.value;
+
 const btn_search = document.querySelector("#search");
 const btn_reset = document.querySelector("#reset");
 
@@ -23,4 +29,22 @@ btn_toggle.onclick = () => {
 	}
 }
 
-// more codes for Search and Reset buttons here
+btn_search.onclick = () => {
+	words = defaultText.split(/[ ]+/);
+	console.log(words);
+	var newText = "";
+	for(let i = 0 ; i < words.length; ++i){
+		if(words[i].length >= parseInt(length.value)){
+			newText += "<span style='background-color:" + color.value + "'>" + words[i] +  "</span>" + " ";
+		}else{
+			newText += words[i] + " ";
+		}
+	}
+	text.innerHTML = newText;
+}
+
+btn_reset.onclick = () => {
+	text.innerHtml = defaultText;
+	length.value = 5;
+	color.value = defColor;
+}

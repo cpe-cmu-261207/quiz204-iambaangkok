a266be9e67b636f95d02fa332a06a0b8f461037c
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-07-29 13:58:27 +0700

    initial commit

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const btn_search = document.querySelector("#search");
+const btn_reset = document.querySelector("#reset");
 
 // define more constants and variables here
+var student_id = 630610756;
+var author_mode = 1;
+var fullname = "BAANGKOK VANIJYANANDA";
 
 btn_toggle.onclick = () => {
-  // your code here
+	if(author_mode == 0){
+		author.innerHTML = "" + student_id + " " + fullname;
+		author_mode = 1;
+		btn_toggle.innerHTML = "Display Calculation";
+	}else{
+		author.innerHTML = "" + (student_id-parseInt(length.value));
+		author_mode = 0;
+		btn_toggle.innerHTML = "Display Author";
+	}
 }
 
 // more codes for Search and Reset buttons here
